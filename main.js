89cb11ec22d10fe45eb23c22f0579429c9065681
j- <jh@live.com.au>
2015-10-04 19:08:24 +1100

    Format as international number before dialing

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 (function () {
 
+/* global phoneUtils */
+
+var settings = {
+	region: 'AU',
+};
+
 var form = document.querySelector('.phone');
 var input = document.querySelector('.phone-input');
 
 form.addEventListener('submit', function (e) {
 	e.preventDefault();
 	var value = input.value;
-	window.location = 'tel:' + value;
+	var international = phoneUtils.formatInternational(value, settings.region);
+	window.location = 'tel:' + international;
 });
 
 })();
