9b56f73755c86bdf4442b368e8716847484931b4
j- <jh@live.com.au>
2015-10-04 19:13:07 +1100

    Add button click handlers

--- main.js
+++ main.js
@@ -6,14 +6,40 @@ var settings = {
 	region: 'AU',
 };
 
+function isValid (input, region) {
+	try {
+		phoneUtils.isValidNumber(input, region);
+		return true;
+	}
+	catch (_) {
+		return false;
+	}
+}
+
+/* Initialization */
+
 var form = document.querySelector('.phone');
 var input = document.querySelector('.phone-input');
+var buttons = document.querySelectorAll('.dialpad-button');
 
-form.addEventListener('submit', function (e) {
+form.addEventListener('submit', handleSubmit);
+
+Array.prototype.forEach.call(buttons, function (button) {
+	button.addEventListener('click', handleButtonClick);
+});
+
+/* Handlers */
+
+function handleSubmit (e) {
 	e.preventDefault();
 	var value = input.value;
 	var international = phoneUtils.formatInternational(value, settings.region);
 	window.location = 'tel:' + international;
-});
+}
+
+function handleButtonClick (e) {
+	e.preventDefault();
+	input.value += this.dataset.input;
+}
 
 })();
