9947e3ab2d32239e986a107f0e0a5487d9d62217
j- <jh@live.com.au>
2015-10-04 19:24:42 +1100

    Add window key up/down handlers

--- main.js
+++ main.js
@@ -16,6 +16,18 @@ function isValid (input, region) {
 	}
 }
 
+function getButton (ch) {
+	ch = String(ch);
+	var result;
+	Array.prototype.forEach.call(buttons, function (button) {
+		if (button.dataset.input === ch) {
+			result = button;
+			return false; // break
+		}
+	});
+	return result || null;
+}
+
 /* Initialization */
 
 var form = document.querySelector('.phone');
@@ -29,6 +41,9 @@ backspace.addEventListener('click', handleBackspace);
 Array.prototype.forEach.call(buttons, function (button) {
 	button.addEventListener('click', handleButtonClick);
 });
+
+window.addEventListener('keydown', handleWindowKeydown);
+window.addEventListener('keyup', handleWindowKeyup);
 
 /* Handlers */
 
@@ -47,6 +62,28 @@ function handleButtonClick (e) {
 function handleBackspace (e) {
 	e.preventDefault();
 	input.value = input.value.substring(0, input.value.length - 1);
+}
+
+function handleWindowKeydown (e) {
+	// Allow special characters
+	if (e.shiftKey) {
+		return;
+	}
+	var key = e.keyCode;
+	var ch = String.fromCharCode(key);
+	var isDigit = ch >= '0' && ch <= '9';
+	if (isDigit) {
+		getButton(ch).classList.add('active');
+	}
+}
+
+function handleWindowKeyup (e) {
+	var key = e.keyCode;
+	var ch = String.fromCharCode(key);
+	var isDigit = ch >= '0' && ch <= '9';
+	if (isDigit) {
+		getButton(ch).classList.remove('active');
+	}
 }
 
 })();
